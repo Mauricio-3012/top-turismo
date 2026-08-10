47404463f71ad3cb6df493400f1bfc2ebc2292a8
Mauricio-3012 <omauriciomoura@gmail.com>
2026-08-10 16:03:55 -0300

    adiciona logout

---
+++ php/logout.php
@@ -0,0 +1,21 @@
+<?php
+session_start();
+
+// Limpa todas as variáveis de sessão
+$_SESSION = [];
+
+// Destrói o cookie de sessão, se existir
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), "", time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destrói a sessão de vez
+session_destroy();
+
+header("Location: ../pages/login.html");
+exit;
+?>
